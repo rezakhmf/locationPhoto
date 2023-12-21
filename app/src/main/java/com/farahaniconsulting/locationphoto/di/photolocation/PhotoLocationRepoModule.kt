5709522c23f431cc0b2b69e8654fe1b14db10297
Reza Farahani <reza.khmf@gmail.com>
2023-12-21 20:18:41 +1100

    update di

--- app/src/main/java/com/farahaniconsulting/locationphoto/di/photolocation/PhotoLocationRepoModule.kt
+++ app/src/main/java/com/farahaniconsulting/locationphoto/di/photolocation/PhotoLocationRepoModule.kt
@@ -4,22 +4,18 @@ import com.farahaniconsulting.locationphoto.data.local.dao.ControlDao
 import com.farahaniconsulting.locationphoto.data.local.dao.PhotoLocationDao
 import com.farahaniconsulting.locationphoto.data.model.repository.LocationPhotoRepositoryImp
 import com.farahaniconsulting.locationphoto.domain.LocationPhotoRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object PhotoLocationRepoModule {
-
-    @Provides
-    fun providePhotoLocationRepository(
-    photoLocationDao: PhotoLocationDao,
-    controlDao: ControlDao
-    ) : LocationPhotoRepository {
-     return  LocationPhotoRepositoryImp(
-         photoLocationDao,
-         controlDao)
-    }
+interface PhotoLocationRepoModule {
+    @Binds
+    fun providePhotoLocationRepositoryIn(
+        locationPhotoRepositoryImp: LocationPhotoRepositoryImp
+    ) : LocationPhotoRepository
 }
